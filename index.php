c1536e8728a59b4ce41a357691597fb0474aeb2b
Jordan <d33gen@outlook.com>
2025-06-03 11:40:32 -0400

    added blog require

--- index.php
+++ index.php
@@ -1,9 +1,13 @@
 <?php
+
 require_once __DIR__ . '/app/Router.php';
 require_once __DIR__ . '/app/controllers/HomeController.php';
+require_once __DIR__ . '/app/controllers/BlogController.php';
 
 $router = new Router();
 
 $router->get('/', [HomeController::class, 'index']);
+$router->get('/blog', [BlogController::class, 'index']);
+$router->get('/blog/embed', [BlogController::class, 'embed']);
 
 $router->resolve();
