eb386b9d1eec93e108f98d7bf999069b549652ac
Pilla <d33gen@outlook.com>
2025-07-16 18:27:34 -0400

    Update BlogController.php

--- app/controllers/BlogController.php
+++ app/controllers/BlogController.php
@@ -2,14 +2,6 @@
 
 class BlogController {
     public function index(): void {
-        $title = 'Blog';
-        ob_start();
-        include __DIR__ . '/../views/blog.php';
-        $content = ob_get_clean();
-        include __DIR__ . '/../views/layout.php';
-    }
-
-    public function embed(): void {
-        include __DIR__ . '/../views/blog.php';
+        require __DIR__ . '/../../views/blog.php';
     }
 }
