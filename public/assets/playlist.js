024d09a9f216f9c8124f2e728f20b1ead3c2e591
Pilla <d33gen@outlook.com>
2025-07-31 14:12:14 -0400

    Create playlist.js

---
+++ public/assets/playlist.js
@@ -0,0 +1,11 @@
+export const playlist = [
+  { title: "4 U", url: "https://files.catbox.moe/9te1b5.mp3" },
+  { title: "Yellow", url: "https://files.catbox.moe/7yydn8.mp3" },
+  { title: "spiral", url: "https://files.catbox.moe/cvkqp0.mp3" },
+  { title: "GOT UR NUMB3R", url: "https://files.catbox.moe/z3pb39.mp3" },
+  {
+    title: "u dont love me or want me",
+    url: "https://cdn.glitch.global/5970b456-17e9-4f56-b0f2-46f0d4636862/u%20dont%20love%20me%20or%20want%20me%20-%20Marluxiam.mp3?v=1746641976716",
+  },
+  { title: "2GOOD4U", url: "https://files.catbox.moe/o98vr5.mp3" },
+];
