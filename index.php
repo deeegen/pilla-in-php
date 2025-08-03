ee9ec261f5dfcb471c5160a2471b8410ea22ba8d
Pilla <d33gen@outlook.com>
2025-08-03 10:07:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -6,13 +6,20 @@ require_once __DIR__ . '/app/controllers/WalkytalkyController.php';
 
 $router = new Router();
 
+// Home page
 $router->get('/', [HomeController::class, 'index']);
 
+// Blog view
 $router->get('/blog', function () {
     require __DIR__ . '/app/views/blog.php';
 });
 
+// Walkytalky: GET for form display, POST for login/post handling
 $router->get('/walkytalky', function () {
+    require __DIR__ . '/app/views/walkytalky.php';
+});
+
+$router->post('/walkytalky', function () {
     require __DIR__ . '/app/views/walkytalky.php';
 });
 
